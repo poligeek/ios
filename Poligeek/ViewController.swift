9f981f3ee132c8989080ce4b9b633c600d4a76f4
Adrien Humiliere <adrien.humiliere@capitainetrain.com>
2016-08-06 14:24:27 +0200

    Initial Commit

---
+++ Poligeek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poligeek
+//
+//  Created by Adrien Humilière on 06/08/2016.
+//  Copyright © 2016 Poligeek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
